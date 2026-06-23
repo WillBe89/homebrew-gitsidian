48a7a1b0989adc757b25268e77818b613befae48
will.be <171881054+WillBe89@users.noreply.github.com>
2026-06-23 12:05:33 +1000

    gitsidian 0.4.8

--- Casks/gitsidian.rb
+++ Casks/gitsidian.rb
@@ -1,12 +1,12 @@
 cask "gitsidian" do
   arch arm: "-arm64", intel: ""
 
-  version "0.4.7"
+  version "0.4.8"
   on_arm do
-    sha256 "8c83eb6469a044318f1898ffbec644d21d4ecef4be0f837e025930ff1885b651"
+    sha256 "06b0757c6a59b7f5f1a6450f48eb4092d149a43bac616fa4c317cba5c1990849"
   end
   on_intel do
-    sha256 "6b3b84ab92440cc5f6813b9fb38a157fe8b2e7e470873754f557419955d57a97"
+    sha256 "3b78b1c490a2db019f45b9020bd85b06c3c7cc6964c4932a7cf01dbe51a416d9"
   end
 
   url "https://github.com/WillBe89/gitsidian/releases/download/v#{version}/Gitsidian-#{version}#{arch}.dmg"
